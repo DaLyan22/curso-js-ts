278d6f83f6fcdf4435a6ec510136fbc6987be9b7
DaLyan22 <gabrielgomessoares2007@gmail.com>
2023-05-25 16:32:39 -0300

    CURSO JS TS

---
+++ js basico/tipos-de-dados/script.js
@@ -0,0 +1,10 @@
+const texto = "aaa" //string
+
+const number1 = 10 //number
+const number2 =10.1 //number
+
+let indefinido; //undefined => sem local de memória
+const nulo = null //null => sem local de memória
+
+const verdade = true //boolean
+
