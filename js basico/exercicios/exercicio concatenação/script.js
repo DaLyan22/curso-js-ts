0d2520594e75fc854c51f419b0adbc6418d9d0d8
DaLyan22 <gabrielgomessoares2007@gmail.com>
2023-05-26 18:28:22 -0300

    CURSO JS TS

---
+++ js basico/exercicios/exercicio concatenação/script.js
@@ -0,0 +1,7 @@
+num1 = prompt("DIGITE UM NÚMERO");
+num2 = prompt("DIGITE OUTRO NÚMERO");
+
+resultado = Number(num1) + Number(num2);
+
+alert("O resultado foi " + resultado);
+alert(`O resultado foi ${resultado}`);
