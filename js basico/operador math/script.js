83040e8d4d02bf7796579f45494d1e93fe5ab590
DaLyan22 <gabrielgomessoares2007@gmail.com>
2023-05-29 21:51:50 -0300

    CURSO JS TS

---
+++ js basico/operador math/script.js
@@ -0,0 +1,5 @@
+const num1 = 18.023293940002
+const num2 = 17
+
+console.log(num1.toFixed(2)) // vai mostrar duas casas decimais arredondando
+console.log(Number.isInteger(num1)) // retorna true ou false se é inteiro
