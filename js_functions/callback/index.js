7e683130e6ee7b48c5ff864143418e9fee4439d6
DaLyan22 <gabrielgomessoares2007@gmail.com>
2023-10-20 23:07:34 -0300

    callback

--- js_functions/callback/index.js
+++ js_functions/callback/index.js
@@ -1,2 +1,45 @@
 //funções executadas quando uma ação ocorre
 
+//gerar número aleatório p exemplo de callback
+function rand(min = 1000, max = 4000){
+   const num = Math.random() * (max - min) + min;
+   return Math.floor(num)
+};
+
+function f1 (cbk){  //cbk = callback
+    setTimeout(function(){
+        console.log('f1');
+        if (cbk) cbk ();
+    }, rand());
+   
+}
+
+function f2 (cbk){
+    setTimeout(function(){
+        console.log('f2');
+        if (cbk) cbk ();
+    }, rand());
+    
+}
+
+function f3 (cbk){
+    setTimeout(function(){
+        console.log('f3');
+        if (cbk) cbk ();
+    }, rand());
+    
+}
+
+f1(f1callback);
+
+function f1callback(){
+    f2(f2callback) //não usa paranteses se não chama imediatamente
+}
+
+function f2callback(){
+    f3(f3callback)
+}
+
+function f3callback(){
+    console.log('deu certo')
+}
