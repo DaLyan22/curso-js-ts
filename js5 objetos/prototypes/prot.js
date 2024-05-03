58d3e2ee520d1eda7cabed45761bfba84c40b50b
DaLyan22 <gabrielgomessoares2007@gmail.com>
2024-05-02 22:06:11 -0300

    js

--- js5 objetos/prototypes/prot.js
+++ js5 objetos/prototypes/prot.js
@@ -1,18 +1,22 @@
+//os prototypes ficam dentro de tds objetos sem vc ter q colocar dentro da função la e pa dai é bom p gastar menos espaço
+
 function Pessoa (nome, sobrenome){
     this.nome = nome;
     this.sobrenome = sobrenome;
-    this.nomeCompleto = () => 'ORIGINAL ' + this.nome + ' ' + this.sobrenome;
+    // this.nomeCompleto = () => 'ORIGINAL ' + this.nome + ' ' + this.sobrenome;
 }
 
-Pessoa.prototype.nomeCompleto = () =>{
-    return this.nome + ' ' + this.sobrenome;
+Pessoa.prototype.nomeCompleto = function(){ 
+    return 'Esse é o nome: ' + this.nome + ' ' + this.sobrenome;
 }
 
 const p1 = new Pessoa('Gabriel', 'Soares');
+const p2 = new Pessoa('Rosa', 'Parks');
+
 console.log(p1.nomeCompleto())
+console.log(p2.nomeCompleto())
 
-const data = new Date();
 
-console.dir(p1);
-console.dir(data);
+
+
 
